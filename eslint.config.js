058a9dc5866055e18159039ab13b0b5a0f6f3e7d
Abe Calder <abecalder1@gmail.com>
2025-11-11 20:45:40 +1300

    fix: package error??

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,22 @@
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import pluginReactHooks from 'eslint-plugin-react-hooks';
-import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import eslintReact from '@eslint-react/eslint-plugin';
+// import pluginReactHooks from 'eslint-plugin-react-hooks';
+// import pluginReactRefresh from 'eslint-plugin-react-refresh';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  {
-    ignores: ['dist', 'node_modules'],
-  },
+  { ignores: ['dist', 'node_modules'] },
+  ...tseslint.configs.recommended,
+  eslintReact.configs.all, // Use the all-in-one config
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    plugins: {
-      react: pluginReact,
-      'react-hooks': pluginReactHooks,
-      'react-refresh': pluginReactRefresh,
-    },
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-      },
-      parser: tseslint.parser,
-    },
-    rules: {
-      ...pluginReact.configs.recommended.rules,
-      ...pluginReactHooks.configs['recommended-latest'].rules,
-      'react-refresh/only-export-components': 'warn',
     },
   },
-  ...tseslint.configs.recommended,
   eslintConfigPrettier,
 ];
