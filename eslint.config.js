3703821ed08ebee8d5c1ce8adb763aa64a56bfa1
Abe Calder <abecalder1@gmail.com>
2025-11-11 16:22:26 +1300

    2nd commit

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import React from 'react'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.eslintrc.cjs', 'vite.config.ts']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -14,10 +15,27 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier,
+      React.configs['recommended']
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    parser: "@typescript-eslint/parser",
+    plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prettier/prettier": "error"
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
